74ace52aece435f042fdcc5d63f40dadbba262e9
Sinthu palanivel <sinthupalanivel1010@gmail.com>
2017-09-02 20:11:43 +0530

    project setup is completed successfully

---
+++ classy-polobackend/src/main/java/com/sindu/classy_polobackend/App.java
@@ -0,0 +1,13 @@
+package com.sindu.classy_polobackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
